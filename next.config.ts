2785dde12910459ebd4d19f769dbdee456cdf2ef
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-11-12 21:26:17 +0330

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  env: {
+    MY_API_URL: process.env.MY_API_URL,
+  },
+
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+      config.plugins.push(new BundleAnalyzerPlugin());
+    }
+    return config;
+  },
+
+  i18n: {
+    locales: ["en", "fa"],
+    defaultLocale: "en",
+  },
 };
 
 export default nextConfig;
